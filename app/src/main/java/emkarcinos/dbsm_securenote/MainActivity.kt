0072cfd60db87604988bb2b5c7a2e98d49b85408
Marcin Kostrzewski <komar86311@gmail.com>
2020-11-05 20:34:01 +0100

    Added register menu navigation

--- app/src/main/java/emkarcinos/dbsm_securenote/MainActivity.kt
+++ app/src/main/java/emkarcinos/dbsm_securenote/MainActivity.kt
@@ -1,11 +1,21 @@
 package emkarcinos.dbsm_securenote
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var registerButton = findViewById<Button>(R.id.registerButton)
+    }
+
+    fun switchToRegisterPage(v: View) {
+        var intent = Intent(this, Register::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/emkarcinos/dbsm_securenote/Register.kt
+++ app/src/main/java/emkarcinos/dbsm_securenote/Register.kt
@@ -1,7 +1,9 @@
 package emkarcinos.dbsm_securenote
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
