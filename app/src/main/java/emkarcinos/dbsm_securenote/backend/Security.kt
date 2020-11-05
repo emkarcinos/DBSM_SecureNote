f2371dd1cbebfd562f4ad9059c6c6a4009ecaf31
Marcin Kostrzewski <komar86311@gmail.com>
2020-11-05 21:09:30 +0100

    Security is now a singleton object

--- app/src/main/java/emkarcinos/dbsm_securenote/backend/Security.kt
+++ app/src/main/java/emkarcinos/dbsm_securenote/backend/Security.kt
@@ -2,13 +2,10 @@ package emkarcinos.dbsm_securenote.backend
 
 import java.security.MessageDigest
 
-class Security {
-    companion object {
-
-        fun generateHash(message: String): String {
-            val bytes = message.toByteArray()
-            val hasher = MessageDigest.getInstance("SHA-256")
-            return hasher.digest(bytes).toString()
-        }
+object Security {
+    fun generateHash(message: String): String {
+        val bytes = message.toByteArray()
+        val hasher = MessageDigest.getInstance("SHA-256")
+        return hasher.digest(bytes).toString()
     }
 }
