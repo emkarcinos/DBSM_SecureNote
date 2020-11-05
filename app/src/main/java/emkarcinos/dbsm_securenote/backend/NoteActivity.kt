99bc24ab34e17e9967d7877f0524b65987d09dc3
Marcin Kostrzewski <komar86311@gmail.com>
2020-11-05 23:41:56 +0100

    Added note activity

---
+++ app/src/main/java/emkarcinos/dbsm_securenote/backend/NoteActivity.kt
@@ -0,0 +1,12 @@
+package emkarcinos.dbsm_securenote.backend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import emkarcinos.dbsm_securenote.R
+
+class NoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note)
+    }
+}
