b9abb2a049e249708e4eaa67b45be711991ad36f
Marcin Kostrzewski <komar86311@gmail.com>
2020-11-05 21:21:06 +0100

    hashing unit test

---
+++ app/src/test/java/emkarcinos/dbsm_securenote/backend/SecurityTest.kt
@@ -0,0 +1,15 @@
+package emkarcinos.dbsm_securenote.backend
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class SecurityTest {
+
+    @Test
+    fun hashingTest() {
+        val passwordRaw = "password"
+        val expectedHash = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"
+        val resultHash = Security.generateHash(passwordRaw)
+        assertSame(expectedHash, resultHash)
+    }
+}
