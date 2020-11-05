4f3dd89086cf2a52913f1b035e8cfbe83c24136d
Marcin Kostrzewski <komar86311@gmail.com>
2020-11-05 22:03:51 +0100

    Now initializes FileManager

--- app/src/main/java/emkarcinos/dbsm_securenote/MainActivity.kt
+++ app/src/main/java/emkarcinos/dbsm_securenote/MainActivity.kt
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import emkarcinos.dbsm_securenote.backend.FileManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var registerButton = findViewById<Button>(R.id.registerButton)
+        // FileManager setup
+        FileManager.directory = applicationContext.filesDir
     }
 
     fun switchToRegisterPage(v: View) {
