374ee4da124e8dde403bf0ec9086555a43a5d75f
Marcin Kostrzewski <komar86311@gmail.com>
2020-11-05 21:27:15 +0100

    generateHash now returns a properly encoded string

--- app/src/main/java/emkarcinos/dbsm_securenote/backend/Security.kt
+++ app/src/main/java/emkarcinos/dbsm_securenote/backend/Security.kt
@@ -6,6 +6,7 @@ object Security {
     fun generateHash(message: String): String {
         val bytes = message.toByteArray()
         val hasher = MessageDigest.getInstance("SHA-256")
-        return hasher.digest(bytes).toString()
+        val digested = hasher.digest(bytes)
+        return digested.fold("", { str, it -> str + "%02x".format(it) })
     }
 }
