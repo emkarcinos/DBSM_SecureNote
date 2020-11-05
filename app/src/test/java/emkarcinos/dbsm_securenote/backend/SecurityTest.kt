2672d87da994127485e2e0fea6294b47cb9d3dad
Marcin Kostrzewski <komar86311@gmail.com>
2020-11-05 21:23:02 +0100

    Replaced assertSame with assertEquals

--- app/src/test/java/emkarcinos/dbsm_securenote/backend/SecurityTest.kt
+++ app/src/test/java/emkarcinos/dbsm_securenote/backend/SecurityTest.kt
@@ -10,6 +10,6 @@ class SecurityTest {
         val passwordRaw = "password"
         val expectedHash = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"
         val resultHash = Security.generateHash(passwordRaw)
-        assertSame(expectedHash, resultHash)
+        assertEquals(expectedHash, resultHash)
     }
 }
