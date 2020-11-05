138762ed58371ebaa7b75abb0a43ecd98c29f845
Marcin Kostrzewski <komar86311@gmail.com>
2020-11-05 22:41:59 +0100

    User creation connected to frontend

--- app/src/main/java/emkarcinos/dbsm_securenote/Register.kt
+++ app/src/main/java/emkarcinos/dbsm_securenote/Register.kt
@@ -4,10 +4,76 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import emkarcinos.dbsm_securenote.backend.FileManager
+import emkarcinos.dbsm_securenote.backend.User
 
 class Register : AppCompatActivity() {
+    private lateinit var usernameBox: EditText
+    private lateinit var password1box: EditText
+    private lateinit var password2box: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        usernameBox = findViewById<EditText>(R.id.newUsernameInputBox)
+        password1box = findViewById<EditText>(R.id.newPasswordInputBox)
+        password2box = findViewById<EditText>(R.id.retypePasswordTextBox)
+    }
+
+    fun submitButtonClick(v: View){
+        if(createUser(v))
+            onSuccessUserCreate()
+    }
+
+    private fun onSuccessUserCreate() {
+        Toast.makeText(this, "Successfully created user.", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun createUser(v: View): Boolean {
+        val username = usernameBox.editableText.toString().trim()
+
+        val password1 = password1box.editableText.toString().trim()
+
+        val password2 = password2box.editableText.toString().trim()
+
+        usernameBox.error = null
+        password1box.error = null
+        password2box.error = null
+        //Check if any of the fields are empty
+        if(username.isEmpty()){
+            usernameBox.setError("Required.")
+            return false
+        }
+
+        if(password1.isEmpty()){
+            password1box.setError("Required.")
+            return false
+        }
+
+        if(password2.isEmpty()){
+            password2box.setError("Required.")
+            return false
+        }
+
+        //Check if two passwords are equal
+        if(password1 != password2){
+            password2box.setError("Passwords do not match.")
+            return false
+        }
+
+        val user = User(username, password2)
+
+        if(!FileManager.saveNewUser(user)){
+            usernameBox.error = "This user alredy exists."
+            return false
+        }
+
+        return true
+
     }
 }
