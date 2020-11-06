14f90c7485ef8d5803cf9fcbae1b96056ef8663e
Marcin Kostrzewski <komar86311@gmail.com>
2020-11-06 11:35:56 +0100

    Implemented file reading / writing

--- app/src/main/java/emkarcinos/dbsm_securenote/backend/FileManager.kt
+++ app/src/main/java/emkarcinos/dbsm_securenote/backend/FileManager.kt
@@ -1,9 +1,13 @@
 package emkarcinos.dbsm_securenote.backend
 
+import android.system.Os
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
-import java.nio.charset.Charset
+import java.io.PrintWriter
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.attribute.BasicFileAttributeView
 
 object FileManager {
     lateinit var directory: File
@@ -44,4 +48,63 @@ object FileManager {
         user.passwordHash = passwordHash
         return user
     }
+
+    /**
+     * Gets file's last modification date.
+     * @return Date as a long integer, or null if the file wasn't found.
+     */
+    fun getFileModificationDate(filename: String): Long? {
+        val file = File(directory, filename)
+        if(!file.exists())
+            return null
+
+        return Os.lstat(file.absolutePath).st_mtime
+    }
+
+    /**
+     * Clears given file's contents, and writes a string into it.
+     */
+    fun saveText(filename: String, string: String) {
+        val file = File(directory, filename)
+        val printer = PrintWriter(file)
+        printer.print(string)
+    }
+
+    /**
+     * Clears given file's contents, and writes an array of strings as lines into it.
+     */
+    fun saveTextLines(filename: String, text: List<String>) {
+        val file = File(directory, filename)
+        val printer = PrintWriter(file)
+        for(line: String in text)
+            printer.println(line)
+    }
+
+    /**
+     * Attempts to read a line from a given file.
+     */
+    fun readText(filename: String): String? {
+        val file = File(directory, filename)
+        val reader = BufferedReader(FileReader(file))
+
+        return reader.readLine()
+    }
+
+    /**
+     * Attempts to read all lines from a given file.
+     */
+    fun readTextLines(filename: String): List<String> {
+        val file = File(directory, filename)
+        val reader = BufferedReader(FileReader(file))
+
+        var lines = ArrayList<String>()
+        var line: String = ""
+        while(line != null){
+            line = reader.readLine()
+            lines.add(line)
+        }
+
+        return lines
+    }
+
 }
