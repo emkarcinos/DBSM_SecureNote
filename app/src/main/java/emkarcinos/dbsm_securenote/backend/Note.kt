f59effed90700783cee75c01ed9cbe0e64c08c87
Marcin Kostrzewski <komar86311@gmail.com>
2020-11-06 13:27:16 +0100

    Added Note unittests

--- app/src/main/java/emkarcinos/dbsm_securenote/backend/Note.kt
+++ app/src/main/java/emkarcinos/dbsm_securenote/backend/Note.kt
@@ -1,4 +1,23 @@
 package emkarcinos.dbsm_securenote.backend
 
-class Note {
+import java.lang.StringBuilder
+
+class Note(var user: User) {
+    lateinit var noteText: String
+
+    constructor(user: User, secret: String): this(user){
+        loadNote(secret)
+    }
+
+    fun saveNote(text: String) {
+        noteText = text
+        FileManager.saveText(Security.generateHash(user.username), noteText)
+    }
+
+    fun loadNote(secret: String) {
+        val text = FileManager.readText(Security.generateHash(user.username))
+        noteText = if(text == null)
+            ""
+        else text
+    }
 }
