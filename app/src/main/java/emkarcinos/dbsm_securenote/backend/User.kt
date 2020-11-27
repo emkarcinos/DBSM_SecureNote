9f7abbc75b34702de943401c91193d7248dbf7de
Marcin Kostrzewski <komar86311@gmail.com>
2020-11-28 00:38:17 +0100

    Primary constructor is now secondary

--- app/src/main/java/emkarcinos/dbsm_securenote/backend/User.kt
+++ app/src/main/java/emkarcinos/dbsm_securenote/backend/User.kt
@@ -2,25 +2,22 @@ package emkarcinos.dbsm_securenote.backend
 
 import java.io.Serializable
 
-class User(username: String, password: String) : Serializable{
-    var username: String = username
-    var password: String = password
+class User : Serializable{
+    constructor(username: String, password: String) {
+        this.username = username
+        this.password = password
+        this.salt = Security.generateSalt()
+        this.passwordHash = Security.generatePBKDF(password, salt)
+    }
+
+    var username: String
+    var password: String
 
     // Salted password (salt is postfix)
     var passwordHash: String
     var salt: String
 
     var note: Note? = null
-
-    /**
-     * Creates a new user.
-     * @param username: User name as string
-     * @param password: Plaintext password
-     */
-    init {
-        this.salt = Security.generateSalt()
-        this.passwordHash = Security.generatePBKDF(password, salt)
-    }
 
     /**
      * Creates a new user instance from already existing user in the database.
@@ -30,7 +27,9 @@ class User(username: String, password: String) : Serializable{
      * @param passwordHash: Hashed and salted password as string
      * @param salt: Salt as string
      */
-    constructor(username: String, passwordHash: String, salt: String) : this(username, "") {
+    constructor(username: String, passwordHash: String, salt: String) {
+        this.username = username
+        this.password = ""
         this.passwordHash = passwordHash
         this.salt = salt
     }
