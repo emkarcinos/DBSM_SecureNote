b5f63cf9dcf9af782804adbbc9d1f7b6a4541add
Marcin Kostrzewski <komar86311@gmail.com>
2020-11-27 19:09:35 +0100

    Note class refactor

--- app/src/main/java/emkarcinos/dbsm_securenote/backend/Note.kt
+++ app/src/main/java/emkarcinos/dbsm_securenote/backend/Note.kt
@@ -2,28 +2,4 @@ package emkarcinos.dbsm_securenote.backend
 
 import java.io.Serializable
 
-class Note(var user: User) : Serializable {
-    lateinit var noteText: String
-
-    constructor(user: User, secret: String): this(user){
-        loadNote(secret)
-    }
-
-    fun saveNote(text: String, secret: String) {
-        noteText = text
-        val cipheredText: ByteArray = Security.encryptString(noteText, secret)
-        FileManager.saveBytes(Security.generateHash(user.username), cipheredText)
-    }
-
-    private fun loadNote(secret: String) {
-        val fileBytes = FileManager.readRawBytes(Security.generateHash(user.username))
-
-        if(fileBytes == null){
-            noteText = ""
-            return
-        }
-        val decipheredText = Security.decryptToString(fileBytes, secret)
-
-        noteText = decipheredText
-    }
-}
+class Note(private val noteText: String, private val user: User) : Serializable
